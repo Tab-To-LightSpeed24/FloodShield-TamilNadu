e39fbaec98fe0a9309223dc0f44c74bf2edfff68
[dyad] <kaushik.vgs@gmail.com>
2025-09-23 00:13:31 +0530

    [dyad] Add React.StrictMode to debug render error - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
 );
